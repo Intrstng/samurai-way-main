0caa6fce8223e8da8d8c42f3eb5fee839a94152e
Intrstng <intrstg@gmail.com>
2024-01-06 16:52:07 +0300

    feat: add store subscriber

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import {addPost, state, StateType, subscribe, updateNewPostText} from './redux/state';
+import {StateType, store} from './redux/state';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {App} from './App';
@@ -10,12 +10,12 @@ export const rerenderEntireTree = (state: StateType) => {
     ReactDOM.render(
         <BrowserRouter>
             <App state={state}
-                 addPost={addPost}
-                 updateNewPostText={updateNewPostText}/>
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root')
     );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,8 +1,6 @@
 import {v1} from 'uuid';
+import {ReactHTML} from 'react';
 
-let rerenderEntireTree = (state: StateType) => {
-    console.log(state)
-}
 
 export type PostItem = {
     id: string
@@ -41,60 +39,78 @@ export type StateType = {
     }
 }
 
-export const state: StateType = {
-    profilePage: {
-        posts: [
-            {id: v1(), message: 'Post 1', likesCount: 14},
-            {id: v1(), message: 'Post 2', likesCount: 11},
-            {id: v1(), message: 'Post 3', likesCount: 17},
-            {id: v1(), message: 'Post 4', likesCount: 9}
-        ],
-        newPostText: ''
+
+
+type StoreType = {
+    _state: StateType
+    getState: () => StateType
+    _callSubscriber: (state: StateType) => void
+    addPost: () => void
+    updateNewPostText: (newText: string) => void
+    subscribe: (observer: any) => void
+}
+
+export const store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: v1(), message: 'Post 1', likesCount: 14},
+                {id: v1(), message: 'Post 2', likesCount: 11},
+                {id: v1(), message: 'Post 3', likesCount: 17},
+                {id: v1(), message: 'Post 4', likesCount: 9}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messages: [
+                {id: v1(), message: 'Hi'},
+                {id: v1(), message: 'How are you?'},
+                {id: v1(), message: 'It`s awesome!'},
+                {id: v1(), message: 'Nice to meet you!'}
+            ],
+            dialogs: [
+                {id: v1(), name: 'Tom', src: 'https://iconape.com/wp-content/png_logo_vector/avatar-10.png'},
+                {id: v1(), name: 'Joe', src: 'https://iconape.com/wp-content/files/ui/10834/png/iconfinder_2_avatar_2754578.png'},
+                {id: v1(), name: 'Ann', src: 'https://iconape.com/wp-content/files/jj/10835/png/iconfinder_4_avatar_2754580.png'},
+                {id: v1(), name: 'Tim', src: 'https://iconape.com/wp-content/files/xf/10838/png/iconfinder_7_avatar_2754582.png'}
+            ]
+        },
+        sidebar: {
+            friends: [
+                {id: v1(), name: 'Jeremy', src: 'https://iconape.com/wp-content/png_logo_vector/avatar-7.png'},
+                {id: v1(), name: 'Kate', src: 'https://iconape.com/wp-content/png_logo_vector/avatar-3.png'},
+                {id: v1(), name: 'Helen', src: 'https://iconape.com/wp-content/files/lv/10837/png/iconfinder_5_avatar_2754581.png'}
+            ]
+        }
     },
-    dialogsPage: {
-        messages: [
-            {id: v1(), message: 'Hi'},
-            {id: v1(), message: 'How are you?'},
-            {id: v1(), message: 'It`s awesome!'},
-            {id: v1(), message: 'Nice to meet you!'}
-        ],
-        dialogs: [
-            {id: v1(), name: 'Tom', src: 'https://iconape.com/wp-content/png_logo_vector/avatar-10.png'},
-            {id: v1(), name: 'Joe', src: 'https://iconape.com/wp-content/files/ui/10834/png/iconfinder_2_avatar_2754578.png'},
-            {id: v1(), name: 'Ann', src: 'https://iconape.com/wp-content/files/jj/10835/png/iconfinder_4_avatar_2754580.png'},
-            {id: v1(), name: 'Tim', src: 'https://iconape.com/wp-content/files/xf/10838/png/iconfinder_7_avatar_2754582.png'}
-        ]
+    getState() {
+        return this._state;
     },
-    sidebar: {
-        friends: [
-            {id: v1(), name: 'Jeremy', src: 'https://iconape.com/wp-content/png_logo_vector/avatar-7.png'},
-            {id: v1(), name: 'Kate', src: 'https://iconape.com/wp-content/png_logo_vector/avatar-3.png'},
-            {id: v1(), name: 'Helen', src: 'https://iconape.com/wp-content/files/lv/10837/png/iconfinder_5_avatar_2754581.png'}
-        ]
+    _callSubscriber(state: StateType) {
+        console.log(state)
+    },
+    addPost() {
+        const newPost = {
+            id: v1(),
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+
+        this._state.profilePage.posts.push(newPost);
+        this.updateNewPostText('');
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    // Проверить типизацию subscribe
+    subscribe(observer: () => void) {
+        this._callSubscriber = observer;
     }
 }
 
-
-export const addPost = () => {
-    const newPost = {
-            id: v1(),
-            message: state.profilePage.newPostText,
-            likesCount: 0
-        };
-    // console.log({...state, profilePage: {...state.profilePage, post: [...state.profilePage.post, newPost]}});
-
-    state.profilePage.posts.push(newPost);
-    updateNewPostText('');
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText: string) => {
-    // !!!!!!!!!!!!!!! //
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer: any) => {
-    rerenderEntireTree = observer;
-}
-
+Object.defineProperty(window, 'store', {
+    value: store,
+    writable: true,
+});
