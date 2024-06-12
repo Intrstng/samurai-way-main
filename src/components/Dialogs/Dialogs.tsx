6b62096c3c7fff36ccf9d258f9e3653753e8ea95
Intrstng <intrstg@gmail.com>
2024-06-12 21:13:50 +0300

    feat: add HOC WithAuthRedirect to DialogsContainer and ProfileContainer to use <Redirect to={'/login'}/> inside these components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,25 +1,14 @@
-import React, {ChangeEvent, FC} from 'react';
+import React, { ChangeEvent, FC } from 'react';
 import S from './Dialogs.module.css';
-import {DialogItem} from './DialogItem/DialogItem';
-import {Message} from './Message/Message';
-import {DialogsItem, MessageItem} from '../../redux/state';
+import { DialogItem } from './DialogItem/DialogItem';
+import { Message } from './Message/Message';
 import { DialogsPropsType } from './DialogsContainer';
-import { Redirect } from 'react-router-dom';
 
-// type DialogsProps = {
-//     dialogs: DialogsItem[]
-//     messages: MessageItem[]
-//     newMessageBody: string
-//     onNewMessageChange: (value: string) => void
-//     onClickSendMessage: () => void
-// }
 
 export const Dialogs: FC<DialogsPropsType> = ({dialogs,
                                            messages,
                                            newMessageBody,
-                                           isCurrentUserAuthorized,
                                            ...props}) => {
-
 
     const dialogsElements = dialogs.map(d => <DialogItem key={d.id} id={d.id} name={d.name} src={d.src}/>);
 
@@ -33,10 +22,6 @@ export const Dialogs: FC<DialogsPropsType> = ({dialogs,
     const onClickHandlerSendMessage = () => {
         props.onClickSendMessage();
     };
-
-    if (!isCurrentUserAuthorized) {
-        return <Redirect to={'/login'}/>
-    }
 
     return (
         <div className={S.dialogs}>
